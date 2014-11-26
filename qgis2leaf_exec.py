fad173559cb3378e19bd8f18f24831765f913cf2
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2014-11-26 09:25:44 +0000

    Style categorized WFS line layers
    Partial fix for #128

--- qgis2leaf_exec.py
+++ qgis2leaf_exec.py
@@ -759,7 +759,7 @@ def qgis2leaf_exec(outputProjectFileName, basemapName, basemapMeta, basemapAddre
 							if i.providerType() == 'WFS' and encode2JSON == False:
 								categories = i.rendererV2().categories()
 								valueAttr = i.rendererV2().classAttribute()
-								categoryStr = "			function doStyle(feature) {"
+								categoryStr = "			function doStyle" + layerName + "(feature) {"
 								categoryStr += "			switch (feature.properties." + valueAttr + ") {"
 								for cat in categories:
 									if not cat.value():
@@ -777,7 +777,7 @@ def qgis2leaf_exec(outputProjectFileName, basemapName, basemapMeta, basemapAddre
 								categoryStr += "}}"
 								stylestr="""
 								pointToLayer: function (feature, latlng) {  
-								return L.circleMarker(latlng, doStyle(feature))
+								return L.circleMarker(latlng, doStyle""" + layerName + """(feature))
 								},
                                 onEachFeature : function (feature, layer) {
                                 """+popFuncs+"""
@@ -844,18 +844,76 @@ def qgis2leaf_exec(outputProjectFileName, basemapName, basemapMeta, basemapAddre
 				feature_group.addLayer(exp_""" + safeLayerName + """JSON);
 				"""		
 						elif i.rendererV2().dump()[0:11] == 'CATEGORIZED' and i.geometryType() == 1:
-							new_obj = """
-				var exp_""" + safeLayerName + """JSON = new L.geoJson(exp_""" + safeLayerName + """,{
-					onEachFeature: pop_""" + safeLayerName + """,
-					style: function (feature) {
-						return {weight: feature.properties.radius_qgis2leaf,
-								color: feature.properties.color_qgis2leaf,
-								opacity: feature.properties.transp_qgis2leaf,
-								fillOpacity: feature.properties.transp_qgis2leaf};
+							layerName=safeLayerName
+							if i.providerType() == 'WFS' and encode2JSON == False:
+								categories = i.rendererV2().categories()
+								valueAttr = i.rendererV2().classAttribute()
+								categoryStr = "			function doStyle" + layerName + "(feature) {"
+								categoryStr += "			switch (feature.properties." + valueAttr + ") {"
+								for cat in categories:
+									if not cat.value():
+										categoryStr += "default: return {"
+									else:
+										categoryStr += "case '" + unicode(cat.value()) + "': return {"
+									#categoryStr += "radius: '" + unicode(cat.symbol().size() * 2) + "',"
+									categoryStr += "color: '" + unicode(cat.symbol().color().name()) + "',"
+									categoryStr += "weight: '" + unicode(cat.symbol().width() * 5) + "',"
+									categoryStr += "opacity: '" + str(cat.symbol().alpha()) + "',"
+									#categoryStr += "fillOpacity: '" + str(cat.symbol().alpha()) + "',"
+									categoryStr +="};"
+									categoryStr += "break;"
+								categoryStr += "}}"
+								stylestr="""style:doStyle""" + layerName + """,
+                                onEachFeature : function (feature, layer) {
+                                """+popFuncs+"""
+                                }
+                                """
+								new_obj="""
+			var """+layerName+"""URL='"""+i.source()+"""&outputFormat=text%2Fjavascript&format_options=callback%3Aget"""+layerName+"""Json';
+			"""+layerName+"""URL="""+layerName+"""URL.replace(/SRSNAME\=EPSG\:\d+/, 'SRSNAME=EPSG:4326');""" + categoryStr + """
+			var exp_"""+layerName+"""JSON = L.geoJson(null, {"""+stylestr+"""}).addTo(map);
+			layerOrder[layerOrder.length] = exp_"""+layerName+"""JSON;
+			var """+layerName+"""ajax = $.ajax({
+					url : """+layerName+"""URL,
+					dataType : 'jsonp',
+					jsonpCallback : 'get"""+layerName+"""Json',
+					contentType : 'application/json',
+					success : function (response) {
+						L.geoJson(response, {
+								onEachFeature : function (feature, layer) {
+									"""+popFuncs+"""
+									exp_"""+layerName+"""JSON.addData(feature)
+								}
+							});
+						for (index = 0; index < layerOrder.length; index++) {
+							map.removeLayer(layerOrder[index]);map.addLayer(layerOrder[index]);
+
+
+
+
+
+
+
+
 						}
-					});
-				feature_group.addLayer(exp_""" + safeLayerName + """JSON);
-				"""		
+					}
+				});
+								"""
+							else:
+								new_obj = """
+					var exp_""" + safeLayerName + """JSON = new L.geoJson(exp_""" + safeLayerName + """,{
+						onEachFeature: pop_""" + safeLayerName + """,
+						style: function (feature) {
+							return {weight: feature.properties.radius_qgis2leaf,
+									color: feature.properties.color_qgis2leaf,
+									opacity: feature.properties.transp_qgis2leaf,
+									fillOpacity: feature.properties.transp_qgis2leaf};
+							}
+						});
+
+					feature_group.addLayer(exp_""" + safeLayerName + """JSON);
+					"""		
+
 						elif i.rendererV2().dump()[0:11] == 'CATEGORIZED' and i.geometryType() == 2:
 							new_obj = """
 				var exp_""" + safeLayerName + """JSON = new L.geoJson(exp_""" + safeLayerName + """,{
