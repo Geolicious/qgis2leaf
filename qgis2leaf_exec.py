156d0de285853cb4a52a2bb12c3e8d7915781a52
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2015-03-09 14:25:02 +0000

    WFS categorized line/polygon pen style
    #171

--- qgis2leaf_exec.py
+++ qgis2leaf_exec.py
@@ -243,6 +243,7 @@ def qgis2leaf_exec(outputProjectFileName, basemapName, basemapMeta, basemapAddre
 						if i.rendererV2().dump()[0:6] == 'SINGLE' and i.geometryType() == 1:
 							color_str = str(i.rendererV2().symbol().color().name())
 							radius_str = str(i.rendererV2().symbol().width() * 5)
+
 							if i.rendererV2().symbol().symbolLayer(0).penStyle() > 1:
 								if i.rendererV2().symbol().symbolLayer(0).penStyle() == 2:
 									penStyle_str = "10,5"
@@ -264,6 +265,8 @@ def qgis2leaf_exec(outputProjectFileName, basemapName, basemapMeta, basemapAddre
 								borderColor_str = str(i.rendererV2().symbol().color().name())
 								radius_str = str(i.rendererV2().symbol().symbolLayer(0).width() * 5)
 							else:
+
+
 								color_str = str(i.rendererV2().symbol().color().name())
 								borderColor_str = str(i.rendererV2().symbol().symbolLayer(0).borderColor().name())
 								if i.rendererV2().symbol().symbolLayer(0).borderStyle() > 1:
@@ -279,6 +282,7 @@ def qgis2leaf_exec(outputProjectFileName, basemapName, basemapMeta, basemapAddre
 							transp_str = str(1 - ( float(i.layerTransparency()) / 100 ) )
 							transp_str2 = str(i.rendererV2().symbol().alpha())
 							if i.rendererV2().symbol().symbolLayer(0).brushStyle() == 0:
+
 								transp_str2 = "0"
 							for line in fileinput.FileInput(dataStore + os.sep + 'exp_' + safeLayerName + '.js',inplace=1):
 								line = line.replace(""""type": "Feature", "properties": { """,""""type": "Feature", "properties": { "color_qgis2leaf": '""" + color_str + """', "border_color_qgis2leaf": '""" + borderColor_str + """', "border_style_qgis2leaf": '""" + borderStyle_str + """', "radius_qgis2leaf": """ + radius_str + """, "transp_qgis2leaf": """ + transp_str + """, "transp_fill_qgis2leaf": """ + transp_str2 + """, """ )
@@ -814,12 +818,22 @@ var crs = new L.Proj.CRS('""" + canvas.mapRenderer().destinationCrs().authid() +
 							if i.providerType() == 'WFS' and encode2JSON == False:
 								color_str = str(i.rendererV2().symbol().color().name())
 								radius_str = str(i.rendererV2().symbol().width() * 5)
+								if i.rendererV2().symbol().symbolLayer(0).penStyle() > 1:
+									if i.rendererV2().symbol().symbolLayer(0).penStyle() == 2:
+										penStyle_str = "10,5"
+									if i.rendererV2().symbol().symbolLayer(0).penStyle() == 3:
+										penStyle_str = "1,5"
+									if i.rendererV2().symbol().symbolLayer(0).penStyle() == 4:
+										penStyle_str = "15,5,1,5"
+									if i.rendererV2().symbol().symbolLayer(0).penStyle() == 5:
+										penStyle_str = "15,5,1,5,1,5"
 								transp_str = str(1 - ( float(i.layerTransparency()) / 100 ) )
 								transp_str2 = str(i.rendererV2().symbol().alpha())
 								stylestr="""
 							style: function (feature) {
 								return {weight: """+radius_str+""",
 										color: '"""+color_str+"""',
+										dashArray: '"""+penStyle_str+"""',
 										opacity: """+transp_str+""",
 										fillOpacity: """+transp_str2+"""};
 								},
@@ -879,6 +893,15 @@ var crs = new L.Proj.CRS('""" + canvas.mapRenderer().destinationCrs().authid() +
 									color_str = str(i.rendererV2().symbol().color().name())
 									borderColor_str = str(i.rendererV2().symbol().symbolLayer(0).borderColor().name())
 									radius_str = str(i.rendererV2().symbol().symbolLayer(0).borderWidth() * 5)
+								if i.rendererV2().symbol().symbolLayer(0).borderStyle() > 1:
+									if i.rendererV2().symbol().symbolLayer(0).borderStyle() == 2:
+										borderStyle_str = "10,5"
+									if i.rendererV2().symbol().symbolLayer(0).borderStyle() == 3:
+										borderStyle_str = "1,5"
+									if i.rendererV2().symbol().symbolLayer(0).borderStyle() == 4:
+										borderStyle_str = "15,5,1,5"
+									if i.rendererV2().symbol().symbolLayer(0).borderStyle() == 5:
+										borderStyle_str = "15,5,1,5,1,5"
 								transp_str = str(1 - ( float(i.layerTransparency()) / 100 ) )
 								transp_str2 = str(i.rendererV2().symbol().alpha())
 								if i.rendererV2().symbol().symbolLayer(0).brushStyle() == 0:
@@ -888,6 +911,7 @@ var crs = new L.Proj.CRS('""" + canvas.mapRenderer().destinationCrs().authid() +
 								return {color: '"""+borderColor_str+"""',
 										fillColor: '"""+color_str+"""',
 										weight: """+radius_str+""",
+										dashArray: '"""+borderStyle_str+"""',
 										opacity: """+transp_str+""",
 										fillOpacity: """+transp_str2+"""};
 								},
@@ -1041,6 +1065,15 @@ var crs = new L.Proj.CRS('""" + canvas.mapRenderer().destinationCrs().authid() +
 									#categoryStr += "radius: '" + unicode(cat.symbol().size() * 2) + "',"
 									categoryStr += "color: '" + unicode(cat.symbol().color().name()) + "',"
 									categoryStr += "weight: '" + unicode(cat.symbol().width() * 5) + "',"
+									if cat.symbol().symbolLayer(0).penStyle() > 1:
+										if cat.symbol().symbolLayer(0).penStyle() == 2:
+											categoryStr += "dashArray: '10,5',"
+										if cat.symbol().symbolLayer(0).penStyle() == 3:
+											categoryStr += "dashArray: '1,5',"
+										if cat.symbol().symbolLayer(0).penStyle() == 4:
+											categoryStr += "dashArray: '15,5,1,5',"
+										if cat.symbol().symbolLayer(0).penStyle() == 5:
+											categoryStr += "dashArray: '15,5,1,5,1,5',"
 									categoryStr += "opacity: '" + str(cat.symbol().alpha()) + "',"
 									#categoryStr += "fillOpacity: '" + str(cat.symbol().alpha()) + "',"
 									categoryStr +="};"
@@ -1104,6 +1137,15 @@ var crs = new L.Proj.CRS('""" + canvas.mapRenderer().destinationCrs().authid() +
 									categoryStr += "fillColor: '" + unicode(cat.symbol().color().name()) + "',"
 									categoryStr += "color: '" + unicode(cat.symbol().symbolLayer(0).borderColor().name()) + "',"
 									categoryStr += "weight: '1',"
+									if cat.symbol().symbolLayer(0).borderStyle() > 1:
+										if cat.symbol().symbolLayer(0).borderStyle() == 2:
+											categoryStr += "dashArray: '10,5',"
+										if cat.symbol().symbolLayer(0).borderStyle() == 3:
+											categoryStr += "dashArray: '1,5',"
+										if cat.symbol().symbolLayer(0).borderStyle() == 4:
+											categoryStr += "dashArray: '15,5,1,5',"
+										if cat.symbol().symbolLayer(0).borderStyle() == 5:
+											categoryStr += "dashArray: '15,5,1,5,1,5',"
 									categoryStr += "opacity: '" + str(cat.symbol().alpha()) + "',"
 									categoryStr += "fillOpacity: '" + str(cat.symbol().alpha()) + "',"
 									categoryStr +="};"
